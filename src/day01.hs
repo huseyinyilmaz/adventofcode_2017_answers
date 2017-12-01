2684d00a260bbac4daf6fc0a1a4065a066911046
Huseyin Yilmaz <yilmazhuseyin@gmail.com>
2017-12-01 01:18:41 -0800

    Updat code to make it more readable.

--- src/day01.hs
+++ src/day01.hs
@@ -2,9 +2,9 @@ module Main where
 import Data.Char (digitToInt)
 main :: IO ()
 main = do
-  cs <- getLine
-  let ns = (fmap (digitToInt) cs)
-  -- zip list with itself but one of the copies has extra first element in the beginning.
-  let pairs = (zip ns ((last ns): ns))
-  let s = (sum . (fmap fst) . (filter (\x-> fst x == snd x))) pairs
-  putStrLn $ show s
+  s <- fmap calculateSum getLine
+  putStrLn s
+  where
+    listToPairs l = (zip l ((last l): l))
+    firstEqSecond (a , b) = a == b
+    calculateSum = show . sum . (fmap fst) . (filter firstEqSecond) . listToPairs . (fmap digitToInt)
