f257e4ab584798da1c9387202d7640b8adf897d7
Huseyin Yilmaz <yilmazhuseyin@gmail.com>
2017-12-01 23:16:24 -0800

    Add second half of the puzzle.

--- src/day01.hs
+++ src/day01.hs
@@ -1,11 +1,35 @@
 module Main where
 import Data.Char (digitToInt)
+import Data.Array
 
-calculateSum :: String -> String
-calculateSum = show . sum . (fmap fst) . (filter firstEqSecond) . listToPairs . (fmap digitToInt)
+readNumbers :: IO [Int]
+readNumbers = getLine >>= return . (fmap digitToInt)
+
+calculateSum :: [Int] -> Int
+calculateSum = sum . (fmap fst) . (filter firstEqSecond) . listToPairs
   where
     listToPairs l = (zip l ((last l): l))
     firstEqSecond (a , b) = a == b
 
+printSum :: [Int] -> IO()
+printSum = putStrLn . show . calculateSum
+
+
+halfSum :: [Int] -> Int
+halfSum ns = sumIndexedList indexedList
+  where
+    size = length ns
+    indexedList = zip [0..size] ns
+    a = array (0, size-1) indexedList
+    nextIndex i = (i + size `div` 2) `mod` size
+    isHalfMatch (i, n)= n == a ! (nextIndex i)
+    sumIndexedList = sum . (fmap snd) . (filter isHalfMatch)
+
+printMatchHalf :: [Int] -> IO()
+printMatchHalf = putStrLn . show . halfSum
+
 main :: IO ()
-main = fmap calculateSum getLine >>= putStrLn
+main = do
+  ns <- readNumbers
+  printSum ns
+  printMatchHalf ns
